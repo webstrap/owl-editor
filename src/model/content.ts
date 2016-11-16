85062268ab7f046754466b077d2e626cbeda4667
Alexander Kong <kong83@gmail.com>
2016-11-16 14:34:36 +0100

    added insertParagraph method to content class

--- src/model/content.ts
+++ src/model/content.ts
@@ -3,10 +3,32 @@
 import { Paragraph } from './paragraph';
 import { Map } from '../utils';
 
-export type ParagraphId = Number;
-
 export class Content 
 {
 	list: Array<Paragraph>;
 	map: Map<Paragraph>;
+
+    constructor()
+    {
+
+    }
+
+    insertParagraph( paragraph: Paragraph, index?: number )
+    {
+        if ( index != null )
+        {
+            if ( 0 <= index && index < this.list.length )
+            {
+                this.list.splice( index, 0, paragraph );
+            }
+            else
+            {
+                console.error( 'Content.insertParagraph: index out of bound' );
+            }
+        }
+        else 
+        {
+            this.list.push( paragraph );
+        }
+    }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,13 @@
 'use strict';
 
-export interface Map<T> {
+export interface Map<T>
+{
     [K: string]: T;
+}
+
+let counter = 0;
+
+export function createId(): string
+{
+    return `c${ ++counter }`;
 }
